402a055bbf356e515bc3e499a59ab8d7787cad30
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-01-27 20:46:50 -0300

    criando o pre save para codificar a senha

--- src/models/User.js
+++ src/models/User.js
@@ -27,4 +27,14 @@ const userSchema = mongoose.Schema({
   }
 });
 
+userSchema.pre("save", async function(next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
+
 module.exports = mongoose.model("User", userSchema);

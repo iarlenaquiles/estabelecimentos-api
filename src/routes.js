12d741b061d0e5e1865d5c7c044e48d90c45c3e9
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-01-27 22:39:25 -0300

    criando as rotas do estabelecimento

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 
 const UserController = require("./controllers/UserController");
 const LoginController = require("./controllers/LoginController");
+const EstabelecimentosController = require("./controllers/EstabelecimentosController");
 
 const auth = require("./middleware/auth");
 
@@ -11,5 +12,11 @@ routes.post("/users", UserController.store);
 routes.post("/login", LoginController.store);
 
 routes.use(auth);
+
+routes.post("/estabelecimentos", EstabelecimentosController.store);
+routes.get("/estabelecimentos", EstabelecimentosController.index);
+routes.delete("/estabelecimentos/:_id", EstabelecimentosController.delete);
+routes.put("/estabelecimentos/:_id", EstabelecimentosController.update);
+routes.get("/estabelecimentos/:_id", EstabelecimentosController.show);
 
 module.exports = routes;

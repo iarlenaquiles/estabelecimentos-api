20d7264dc2c8633f8e2951e7a71cea7cba255fec
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-01-27 21:31:05 -0300

    finalizando cadastro do user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,13 +3,13 @@ const User = require("../models/User");
 module.exports = {
   async store(req, res) {
     try {
-      const user = new User(req.body);
-      await user.save();
+      const user = await User.create(req.body);
+
       const token = await user.generateAuthToken();
 
       res.status(201).json({ user, token });
     } catch (error) {
-      res.status(400).json({ error });
+      res.status(400).json({ error: "Ivalid user data" });
     }
   }
 };
